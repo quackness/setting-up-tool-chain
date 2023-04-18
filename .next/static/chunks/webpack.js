ba93e8c6e6466a5c4884e655a668ba5ed8e86230
Karolina Redden <dubajkaro@gmail.com>
2023-04-18 09:46:51 -0300

    add mouse count and chnage the browser title based on which speaker is moused over

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "01bcbbfef2f2355a03bb";
+/******/ 	var hotCurrentHash = "4e70b650db658e4710b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/ImageChangeOnScroll.js
+++ pages/ImageChangeOnScroll.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageToggleOnScroll from '../src/ImageToggleOnScroll';
 
 const ImageChangeOnScroll = () => {
+
+  const [currentSpeakerId, setCurrentSpeakerId] = useState(0);
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+
+  useEffect(() => {
+    window.document.title = `Speaker Id: ${currentSpeakerId}`;
+    console.log(`useEff: ${currentSpeakerId}`)
+  });
+
   return (
     <div>
+      <span>mouseEventCnt: ${mouseEventCnt}</span>
       {[1124, 187, 823, 1269, 1530].map((speakerId) => {
         return (
-          <div key={speakerId}>
+          <div key={speakerId}
+            onMouseOver={() => {
+              setCurrentSpeakerId(speakerId);
+              setMouseEventCnt(mouseEventCnt + 1);
+
+            }}>
             <ImageToggleOnScroll
               primaryImg={`/static/speakers/bw/Speaker-${speakerId}.jpg`}
               secondaryImg={`/static/speakers/Speaker-${speakerId}.jpg`}
